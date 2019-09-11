37ded94a4e0435425cfaf4f58da717fcfd1faf1b
Kemberly Eliscar <kemberlyeliscar@Kemberlys-Air-3.home>
2019-09-11 11:01:43 -0400

    refactored my classes into function components

---
+++ reducer-todo/src/components/Todo.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { initialState, titleReducer } from "../reducers/titleReducer";
+
+const Title = () => {
+    const [newTitleText, setNewTitleText] = useState("");
+    const [state, dispatch] = useReducer(titleReducer, initialState);
+  
+    const handleChanges = e => {
+      setNewTitleText(e.target.value);
+    };
+  
+    return (
+      <div>
+        {!state.editing ? (
+          <h1>
+            {state.title}{" "}
+            <i
+              className="far fa-edit"
+              onClick={() => dispatch({ type: "TOGGLE_EDITING" })}
+            />
+          </h1>
+        ) : (
+          <div>
+            <input
+              className="title-input"
+              type="text"
+              name="newTitleText"
+              value={newTitleText}
+              onChange={handleChanges}
+            />
+            <button
+              onClick={() => {
+                dispatch({ type: "UPDATE_TITLE", payload: newTitleText });
+                setNewTitleText("");
+              }}
+            >
+              Update title
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  };
+  
+  export default Title;
+  
